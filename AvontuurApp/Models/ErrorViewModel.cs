73463a44799f9c2dcd84bd1fea1d51d419b9eb6d
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2025-03-07 05:36:55 +0200

    Add project files.

---
+++ AvontuurApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AvontuurApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
