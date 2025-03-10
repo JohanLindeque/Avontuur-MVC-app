62321834ce1f2c98a06aab92f35f4fa780b213a6
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2025-03-10 22:43:49 +0200

    'seeding avontuur entries table' = init data

--- AvontuurApp/Data/ApplicationDbContext.cs
+++ AvontuurApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,35 @@ namespace AvontuurApp.Data
         }
 
         public DbSet<AvontuurEntry> AvontuurEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AvontuurEntry>().HasData(
+                new AvontuurEntry { 
+                    Id = 1, 
+                    Title = "Eerste overland kamp", 
+                    Content = "1ste kamp met die nuwe 4x4 by 3 berge.", 
+                    Location = "3 Berge kamp", 
+                    CreatedAt = DateTime.Now 
+                },
+                new AvontuurEntry { 
+                    Id = 2, Title = "Eerste 4x4", 
+                    Content = "1ste 4x4 trippie.", 
+                    Location = "Pilansberg", 
+                    CreatedAt = DateTime.Now 
+                },
+                 new AvontuurEntry
+                 {
+                     Id = 3,
+                     Title = "Uitkyk",
+                     Content = "Caos, grade 4 trail",
+                     Location = "3 Berge",
+                     CreatedAt = DateTime.Now
+                 }
+                );
+
+        }
     }
 }
