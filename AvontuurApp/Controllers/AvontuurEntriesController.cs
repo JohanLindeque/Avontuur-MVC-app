7523b36d49c33c661e10d4b3d7504187a4bb71f0
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2025-03-10 22:31:21 +0200

    added Avontuur entries view and nav bar link

---
+++ AvontuurApp/Controllers/AvontuurEntriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvontuurApp.Controllers
+{
+    public class AvontuurEntriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
