f66d95520ce4230df23407e17ac2a9edda613f54
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2025-03-11 06:12:00 +0200

    get data from db and display

--- AvontuurApp/Controllers/AvontuurEntriesController.cs
+++ AvontuurApp/Controllers/AvontuurEntriesController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AvontuurApp.Data;
+using AvontuurApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AvontuurApp.Controllers
 {
     public class AvontuurEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public AvontuurEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<AvontuurEntry> objAvontuurEntryList = _db.AvontuurEntries.ToList();
+
+            return View(objAvontuurEntryList);
         }
     }
 }
