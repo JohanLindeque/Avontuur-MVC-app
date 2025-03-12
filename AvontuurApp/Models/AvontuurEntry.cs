e5211dc590b87acbccc77922d4c0cdaf3c636ebf
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2025-03-12 06:57:36 +0200

    add client side validation

--- AvontuurApp/Models/AvontuurEntry.cs
+++ AvontuurApp/Models/AvontuurEntry.cs
@@ -6,11 +6,12 @@ namespace AvontuurApp.Models
     {
         public int Id { get; set; }
         [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title needs to be between 3 and 100 characters!")]
         public string Title { get; set; } = string.Empty;
         [Required]
         public string Content { get; set; } = string.Empty;
         public string Location { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Select a vaild date!")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
